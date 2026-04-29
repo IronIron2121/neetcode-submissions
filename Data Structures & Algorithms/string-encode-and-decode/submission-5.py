20d0dfade8f455c66a04fdea9e132145071581a7
Gabriel Okafor <81721606+IronIron2121@users.noreply.github.com>
2026-04-29 21:46:07 +0100

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,55 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        out = ""
+        sizes = []
+        for string in strs:
+            sizes.append(len(string))
+        for size in sizes:
+            out += str(size)
+            out += ';'
+        out += '#'
+        for string in strs:
+            out += string
+
+        # The key thing is that you place your information at the BEGINNING of string
+        # This way, 
+        return out
+
+    def decode(self, s: str) -> List[str]:
+        if not s:
+            print('No S')
+            return []
+
+        out = []
+        sizes = []
+        char = ''
+        index = 0
+        char = s[index]
+        
+        while char != '#':
+            thisSize = ''
+
+            while char != ';':
+                thisSize += char
+                index += 1
+                char = s[index]
+
+            sizes.append(int(thisSize))
+            index += 1
+            char = s[index]
+
+        
+        strStart = sum(sizes)
+
+        index += 1
+        index2 = 0
+        while index2 < len(sizes):
+            out.append(s[index : index + sizes[index2]])
+            index = index + sizes[index2]
+            index2+=1
+
+        return out
